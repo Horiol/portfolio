393275954d790466d89cdb92f5ee62ec69948b87
Oriol Vall <oriol.vall.hdez@gmail.com>
2018-09-04 21:09:49 +0200

    vue-analytics added

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,20 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
+
+const isProd = process.env.NODE_ENV === 'production';
+
+Vue.use(VueAnalytics, {
+  id: 'UA-125204258-1',
+  router,
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd,
+  },
+});
 
 new Vue({
   router,
